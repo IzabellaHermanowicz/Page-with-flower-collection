a6535fbe9503d43baee4a7c5a948fa982eddf36d
IzabellaHe <izabella.hermanowicz@gmail.com>
2019-08-11 15:25:59 +0200

    Adding comments and file with test data

--- v3/app.js
+++ v3/app.js
@@ -1,34 +1,15 @@
 let express = require("express"),
     app = express(),
     port = process.env.PORT || 3000,
-    bodyParser = require("body-parser")
-    mongoose = require("mongoose")
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    Flowertype = require("./models/flowertype"),
+    seedDB = require("./seeds");
 
-mongoose.connect("mongodb://localhost:27017/flower_types", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/new_flower_types", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
-
-var flowertypeSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-});
-
-var Flowertype = mongoose.model("Flowertype", flowertypeSchema);
-
-/*Flowertype.create(
-    {
-        name:"Rose",
-        image:"https://images.unsplash.com/photo-1531874824027-2a0d33bd6338?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
-        description:"All roses were originally wild and they come from several parts of the world, North America, Europe, northwest Africa and many parts of Asia and Oceania. There are over 100 different species of roses."
-    }, function(err, flowertype){
-       if(err){
-           console.log(err);
-       } else{
-           console.log("NEW FLOWER");
-           console.log(flowertype);
-       }
-});*/
+seedDB();
 
 app.get("/", function(req, res){
     res.render("landing");
@@ -66,7 +47,7 @@ app.get("/flowertypes/new", function(req, res){
 });
 
 app.get("/flowertypes/:id", function(req,res){
-    Flowertype.findById(req.params.id, function(err, foundFlowertype){
+    Flowertype.findById(req.params.id).populate("comments").exec(function(err, foundFlowertype){
         if(err){
             console.log(err);
         }else{
---
+++ v3/seeds.js
@@ -0,0 +1,64 @@
+var mongoose = require("mongoose");
+var Flowertype = require("./models/flowertype");
+var Comment   = require("./models/comment");
+ 
+var data = [
+    {
+        name: "Peony", 
+        image: "https://images.unsplash.com/photo-1527061011665-3652c757a4d4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    },
+    {
+        name: "Cymbidium Orchid", 
+        image: "https://images.unsplash.com/photo-1542407242725-7d7f3d865665?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    },
+    {
+        name: "Rose", 
+        image: "https://images.unsplash.com/photo-1531874824027-2a0d33bd6338?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    }
+]
+ 
+function seedDB(){
+   //Remove all Flowertypes
+   Flowertype.remove({}, function(err){
+        if(err){
+            console.log(err);
+        }
+        console.log("removed flowertypes!");
+        Comment.remove({}, function(err) {
+            if(err){
+                console.log(err);
+            }
+            console.log("removed comments!");
+             //add a few Flowertypes
+            data.forEach(function(seed){
+                Flowertype.create(seed, function(err, flowertype){
+                    if(err){
+                        console.log(err)
+                    } else {
+                        console.log("added a flowertype");
+                        //create a comment
+                        Comment.create(
+                            {
+                                text: "Love the color of this flower!",
+                                author: "Homer"
+                            }, function(err, comment){
+                                if(err){
+                                    console.log(err);
+                                } else {
+                                    flowertype.comments.push(comment);
+                                    flowertype.save();
+                                    console.log("Created new comment");
+                                }
+                            });
+                    }
+                });
+            });
+        });
+    }); 
+    //add a few comments
+}
+ 
+module.exports = seedDB;
