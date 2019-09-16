68c95f334d6d785eacb875e3705cf90aa6f08aab
IzabellaHe <izabella.hermanowicz@gmail.com>
2019-09-16 18:27:43 +0200

    Signup and login
    User has to be logged to add comments, page won't show some actions to not login users, checking if person is already signin

--- code/app.js
+++ code/app.js
@@ -3,14 +3,36 @@ let express = require("express"),
     port = process.env.PORT || 3000,
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
     Flowertype = require("./models/flowertype"),
     Comment = require("./models/comment"),
+    User = require("./models/user"),
     seedDB = require("./seeds");
 
 mongoose.connect("mongodb://localhost:27017/flower_types_4", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 seedDB();
+
+//passport
+app.use(require("express-session")({
+    secret:"Emmecik mnie kocha przynajmniej mam nadzieje",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
 
 app.get("/", function(req, res){
     res.render("landing");
@@ -22,7 +44,7 @@ app.get("/flowertypes", function(req, res){
        if(err){
            console.log(err);
        }else{
-            res.render("flowertypes/index", {flowertypes:allFlowertypes});
+            res.render("flowertypes/index", {flowertypes:allFlowertypes, currentUser: req.user});
        }
    });
 });
@@ -58,7 +80,7 @@ app.get("/flowertypes/:id", function(req,res){
 });
 
 //COMMENTS
-app.get("/flowertypes/:id/comments/new", function(req, res) {
+app.get("/flowertypes/:id/comments/new",isLoggedIn, function(req, res) {
     Flowertype.findById(req.params.id, function(err, flowertype){
        if(err){
            console.log(err);
@@ -68,7 +90,7 @@ app.get("/flowertypes/:id/comments/new", function(req, res) {
     })
 });
 
-app.post("/flowertypes/:id/comments", function(req, res) {
+app.post("/flowertypes/:id/comments", isLoggedIn, function(req, res) {
     Flowertype.findById(req.params.id, function(err, flowertype){
         if(err){
             console.log(err);
@@ -86,6 +108,48 @@ app.post("/flowertypes/:id/comments", function(req, res) {
         }
      })
 });
+
+//auth routes
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.post("/register", function(req, res) {
+    var newUser = new User({username: req.body.username})
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req,res, function(){
+            res.redirect("/flowertypes");
+        });
+    });
+});
+
+//login
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.post("/login",passport.authenticate("local", 
+    {successRedirect:"/flowertypes",
+    failureRedirect: "/login"
+    }), 
+    function(req, res) {
+});
+
+app.get("/logout", function(req, res) {
+   req.logout();
+   res.redirect("/flowertypes"); 
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+};
 
 app.listen(port, function(){
     console.log("START");
